0a9bb2a158128c4cdc6cf37d5fd6028afdc483af
Marvin Arnold <marvinmarnold@gmail.com>
2013-11-05 20:06:50 +0100

    Index locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,23 @@
 class Location < ActiveRecord::Base
-   has_ancestry
+  has_ancestry
+
+  before_update :index_if_changed
+  after_create :index
 
   def to_s
     name
+  end
+
+private
+
+  def index_if_changed
+    if self.name_changed?
+      SearchSuggestion.unindex_phrase(name_was)
+      SearchSuggestion.index_phrase(name)
+    end
+  end
+
+  def index
+    SearchSuggestion.index_phrase(name)
   end
 end
