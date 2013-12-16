13adc2649420e8d731273c3a6ad3ebc39d70a9ec
Dhiraj bajaj <dhirajbajaj50@gmail.com>
2013-12-16 16:14:04 +0000

    remove from gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131205103643) do
+ActiveRecord::Schema.define(version: 20131216104524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -312,6 +312,8 @@ ActiveRecord::Schema.define(version: 20131205103643) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "banner"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "shops", ["location_id"], name: "index_shops_on_location_id", using: :btree
