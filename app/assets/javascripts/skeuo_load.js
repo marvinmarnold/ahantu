fe5c023822684d1063869a7c03a08279fed30f58
dhirajbajaj <dhirajbajaj50@gmail.com>
2014-02-06 20:42:59 +0530

    skeuo_load.js added

---
+++ app/assets/javascripts/skeuo_load.js
@@ -0,0 +1,17 @@
+loadSkeo = function(){
+    if(typeof window.card === 'undefined') {
+      window.card = new Skeuocard($("#skeuocard"));
+    }
+  };
+validateform = function(){
+  var resp = card.isValid();
+  if(resp) {
+    $('#credit_card_form').submit();
+    return true;
+  }
+  // alert("Kindly Correct all Credit Card Details");
+  return false;
+};
+
+$(document).on('ready', loadSkeo);
+$(document).on('page:load', loadSkeo);
