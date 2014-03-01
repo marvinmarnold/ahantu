90b3e844f5c8c2e827f7b123e4be57fbcc77450a
Marvin Arnold <marvinmarnold@gmail.com>
2014-03-02 00:28:05 +0100

    Cleaned up cc form

--- app/helpers/billing_informations_helper.rb
+++ app/helpers/billing_informations_helper.rb
@@ -3,4 +3,7 @@ module BillingInformationsHelper
     CreditCard::CREDIT_CARD_TYPES
   end
 
+  def credit_card_expiration(credit_card)
+    DateTime.new(credit_card.year, credit_card.month, 1).strftime("%m/%y")
+  end
 end
